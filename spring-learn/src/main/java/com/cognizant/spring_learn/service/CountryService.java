020d6d3e9f2e20e3ef3d75f972e65b5498887fe7
Abuosm <abubakerosman56@gmail.com>
2026-07-01 12:55:27 +0530

    Added REST API handson updated

---
+++ spring-learn/src/main/java/com/cognizant/spring_learn/service/CountryService.java
@@ -0,0 +1,33 @@
+package com.cognizant.spring_learn.service;
+
+import java.util.ArrayList;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Service;
+
+import com.cognizant.spring_learn.Country;
+
+@Service
+public class CountryService {
+
+    public Country getCountry(String code) {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("country.xml");
+
+        ArrayList<Country> countries =
+                context.getBean("countryList", ArrayList.class);
+
+        for (Country country : countries) {
+
+            if (country.getCode().equalsIgnoreCase(code)) {
+
+                return country;
+            }
+
+        }
+
+        return null;
+    }
+}
